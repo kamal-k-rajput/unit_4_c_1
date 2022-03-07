3172e77af36be932c4034bcbeb53c9494ce36521
kamal-kishor-rajput <90085564+kamal-kishor-rajput@users.noreply.github.com>
2022-03-07 12:11:51 +0530

    added two middleware

--- index.js
+++ index.js
@@ -2,23 +2,39 @@ const express = require("express");
 
 const app = express();
 
-
+app.use(logger);
 app.get("/", (req, res) => {
   res.send("this is home page");
 });
 app.get("/books", (req, res) => {
   res.send("this is book page");
+  console.log(req.path);
 });
-app.get("/libraries", (req, res) => {
+app.get("/libraries", checkPermission("librarian"), (req, res) => {
   res.send("this is libraries page");
+  console.log(req.path);
 });
-app.get("/authors", (req, res) => {
+app.get("/author", checkPermission("author"), (req, res) => {
   res.send("this is authors page");
+  console.log(req.path);
 });
 
-app.use((req, res) => {
-  res.send("hello");
-});
+function logger(req, res, next) {
+  console.log("before the logger");
+  next();
+  console.log("after the logger");
+}
+function checkPermission(data) {
+  return function logger1(req, res, next) {
+    if (data === "librarian") {
+      return next();
+    } else if (data === "author") {
+      return next();
+    } else {
+      return res.send("You are not allowed to access this page.");
+    }
+  };
+}
 
 app.listen(8080, function () {
   console.log("Listning to the port 8080.");
